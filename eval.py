45104f5b03a8eaab95c0515cd1bb77f6c56ddb78
kenmaz <kenmaz@mercari.com>
2019-06-11 11:29:55 +0900

    fix

--- eval.py
+++ eval.py
@@ -1,6 +1,16 @@
 import turicreate as tc
-m=tc.load_model('sign.model')
+import os
+
+m=tc.load_model('model.model')
 data=tc.image_analysis.load_images('test')
-p=m.predict(data['image'], confidence_threshold=0.001, iou_threshold=1)
+
+p=m.predict(data['image'], confidence_threshold=0.01)
+
 res=tc.one_shot_object_detector.util.draw_bounding_boxes(data['image'],p)
 res.explore()
+
+if not os.path.isdir('out'):
+    os.makedirs('out')
+for i,img in enumerate(res):
+    img.save("out/out_%s.png" % i)
+
--- train.py
+++ train.py
@@ -3,5 +3,5 @@ import turicreate as tc
 imgs = tc.load_images('imgs')
 imgs['label'] = imgs['path'].element_slice(5, -4)
 model = tc.one_shot_object_detector.create(imgs, 'label')
-model.save('sign.model')
+model.save('model.model')
 
